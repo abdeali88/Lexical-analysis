e86f65a3be46ba858a567d36bd7097352bcbda86
abdeali88 <38809496+abdeali88@users.noreply.github.com>
2020-01-20 22:25:29 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,23 +1,23 @@
 import re
 
-code_file = open('InputProg.c','r')
-builtin_file = open('builtin.txt', 'r')
+code_file = open('c_prog.c','r')
+built_in_file = open('built_in.txt', 'r')
 keywords_file = open('keywords.txt', 'r')
 operators_file = open('operators.txt', 'r')
-special_file = open('special.txt', 'r')
+special_symbol_file = open('special_symbol.txt', 'r')
 
 code = code_file.read()
-builtin = builtin_file.read()
+built_in = built_in_file.read()
 keywords = keywords_file.read()
 operators = operators_file.read()
-special = special_file.read()
+special_symbol = special_symbol_file.read()
 
 count = 0
 code_lines = code.split('\n')
-builtin = builtin.split('\n')
+built_in = built_in.split('\n')
 keywords = keywords.split('\n')
 operators = operators.split('\n')
-special = special.split('\n')
+special_symbol = special_symbol.split('\n')
 sym = list()
 
 for code_line in code_lines:
@@ -38,14 +38,14 @@ for code_line in code_lines:
     for code_token in code_tokens:
         if code_token[-1] == ',':
             comma = True
-        if code_token in builtin:
-            print('Bfun#{}'.format(builtin.index(code_token) +1), end=' ')
+        if code_token in built_in:
+            print('Bfun#{}'.format(built_in.index(code_token) +1), end=' ')
         elif code_token in keywords:
             print('keyword#{}'.format(keywords.index(code_token)+1), end=' ')
         elif code_token in operators:
             print('oper#{}'.format(operators.index(code_token)+1), end=' ')
-        elif code_token in special:
-            print('spl#{}'.format(special.index(code_token)+1), end=' ')
+        elif code_token in special_symbol:
+            print('spl#{}'.format(special_symbol.index(code_token)+1), end=' ')
         else:
             if code_token in sym:
                 print('sym#{}'.format(sym.index(code_token)+1), end=' ')
@@ -56,7 +56,7 @@ for code_line in code_lines:
     print('\n\n')
 
 code_file.close()
-builtin_file.close()
+built_in_file.close()
 keywords_file.close()
 operators_file.close()
-special_file.close()
+special_symbol_file.close()
