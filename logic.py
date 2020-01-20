bdf534541ad578cdfcd12ecdfdc13cd5b8681cce
abdeali88 <38809496+abdeali88@users.noreply.github.com>
2020-01-20 22:40:48 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,23 +1,23 @@
 import re
 
-code_file = open('c_prog.c','r')
-built_in_file = open('built_in.txt', 'r')
+code_file = open('InputProg.c','r')
+builtin_file = open('builtin.txt', 'r')
 keywords_file = open('keywords.txt', 'r')
 operators_file = open('operators.txt', 'r')
-special_symbol_file = open('special_symbol.txt', 'r')
+special_file = open('special.txt', 'r')
 
 code = code_file.read()
-built_in = built_in_file.read()
+builtin = builtin_file.read()
 keywords = keywords_file.read()
 operators = operators_file.read()
-special_symbol = special_symbol_file.read()
+special = special_file.read()
 
 count = 0
 code_lines = code.split('\n')
-built_in = built_in.split('\n')
+builtin = builtin.split('\n')
 keywords = keywords.split('\n')
 operators = operators.split('\n')
-special_symbol = special_symbol.split('\n')
+special = special.split('\n')
 sym = list()
 
 for code_line in code_lines:
@@ -34,18 +34,19 @@ for code_line in code_lines:
             code_line += ' ;'
     
     code_tokens = code_line.split(' ')
+    # print(code_tokens)
     
     for code_token in code_tokens:
         if code_token[-1] == ',':
-            comma = True
-        if code_token in built_in:
-            print('Bfun#{}'.format(built_in.index(code_token) +1), end=' ')
+            code_token = code_token[:-1] 
+        if code_token in builtin:
+            print('Bfun#{}'.format(builtin.index(code_token) +1), end=' ')
         elif code_token in keywords:
             print('keyword#{}'.format(keywords.index(code_token)+1), end=' ')
         elif code_token in operators:
             print('oper#{}'.format(operators.index(code_token)+1), end=' ')
-        elif code_token in special_symbol:
-            print('spl#{}'.format(special_symbol.index(code_token)+1), end=' ')
+        elif code_token in special:
+            print('spl#{}'.format(special.index(code_token)+1), end=' ')
         else:
             if code_token in sym:
                 print('sym#{}'.format(sym.index(code_token)+1), end=' ')
@@ -54,9 +55,17 @@ for code_line in code_lines:
                 print('sym#{}'.format(len(sym) - 1),  end=' ')
 
     print('\n\n')
+print(sym)
+string = '\n'.join(sym)
+print(string)
+
+symbols_file = open("symbols.txt","w+")
+symbols_file.write(string)
+
 
 code_file.close()
-built_in_file.close()
+builtin_file.close()
 keywords_file.close()
 operators_file.close()
-special_symbol_file.close()
+special_file.close()
+symbols_file.close()
